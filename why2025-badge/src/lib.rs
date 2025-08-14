0a819a1c1acf7dafb5d527c3baddf3178365e62f
Benjamin Stürz <benni@stuerz.xyz>
2025-08-14 17:27:02 +0200

    add why2025-badge

---
+++ why2025-badge/src/lib.rs
@@ -0,0 +1,4 @@
+#![no_std]
+
+pub mod stdio;
+pub mod malloc;
---
+++ why2025-badge/src/stdio.rs
@@ -0,0 +1,34 @@
+use core::{fmt, ffi::c_void};
+
+struct Stdio;
+
+impl fmt::Write for Stdio {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        unsafe {
+            why2025_badge_sys::fwrite(
+                s.as_ptr() as *mut c_void,
+                1,
+                s.len() as u32,
+                why2025_badge_sys::stdout,
+            );
+        }
+        Ok(())
+    }
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use fmt::Write;
+    Stdio.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => { $crate::stdio::_print(format_args!($($arg)*)) };
+}
+
+#[macro_export]
+macro_rules! println {
+    () => { print!("\n") };
+    ($($arg:tt)*) => { print!("{}\n", format_args!($($arg)*)) };
+}

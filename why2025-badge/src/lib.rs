184d9439f5619225570aaba4985f6f5e3b4b69fa
Benjamin Stürz <benni@stuerz.xyz>
2025-08-14 23:13:51 +0200

    minor fixes

--- why2025-badge/src/lib.rs
+++ why2025-badge/src/lib.rs
@@ -1,4 +1,6 @@
 #![no_std]
 
+pub use why2025_badge_sys as ffi;
+
 pub mod stdio;
 pub mod malloc;
--- why2025-badge/src/stdio.rs
+++ why2025-badge/src/stdio.rs
@@ -29,6 +29,6 @@ macro_rules! print {
 
 #[macro_export]
 macro_rules! println {
-    () => { print!("\n") };
-    ($($arg:tt)*) => { print!("{}\n", format_args!($($arg)*)) };
+    () => { $crate::print!("\n") };
+    ($($arg:tt)*) => { $crate::print!("{}\n", format_args!($($arg)*)) };
 }

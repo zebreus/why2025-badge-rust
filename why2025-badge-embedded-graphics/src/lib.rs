63d1662f6c35d10e699602b1c8cf1a4932e6b68c
Zebreus <zebreus@zebre.us>
2025-08-18 00:55:52 +0200

    Add more features to the embedded graphics driver

--- why2025-badge-embedded-graphics/src/lib.rs
+++ why2025-badge-embedded-graphics/src/lib.rs
@@ -1,13 +1,16 @@
 #![no_std]
 extern crate alloc;
 
+use alloc::ffi::CString;
 use alloc::string::{String, ToString};
 use core::ptr::null_mut;
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::{geometry::Size, pixelcolor::Rgb888, prelude::*};
 use why2025_badge_sys::{
-    framebuffer_t, pixel_format_t, window_create, window_flag_t, window_framebuffer_create,
-    window_handle_t, window_present, window_size_t,
+    framebuffer_t, pixel_format_t, window_coords_t, window_create, window_flag_t, window_flags_get,
+    window_flags_set, window_framebuffer_create, window_framebuffer_size_set, window_handle_t,
+    window_position_get, window_position_set, window_present, window_size_set, window_size_t,
+    window_title_set,
 };
 
 const fn rgb888_to_rgb565(r: u8, g: u8, b: u8) -> u16 {
@@ -16,8 +19,7 @@ const fn rgb888_to_rgb565(r: u8, g: u8, b: u8) -> u16 {
 }
 
 pub struct Why2025BadgeWindow {
-    size: (i32, i32),
-    title: [u8; 64],
+    title: String,
     window: window_handle_t,
     framebuffer: *mut framebuffer_t,
 }
@@ -75,21 +77,19 @@ impl Why2025BadgeWindow {
         };
         let title_bytes = config.title.as_bytes();
         assert!(
-            title_bytes.len() < 63,
-            "Title must be less than 63 bytes long"
+            title_bytes.len() < 127,
+            "Title must be less than 127 bytes long"
         );
 
         let mut this = Why2025BadgeWindow {
-            size: (size.w, size.h),
-            title: [0; 64],
+            title: config.title,
             window: null_mut(),
             framebuffer: null_mut(),
         };
-        this.title[..title_bytes.len()].copy_from_slice(title_bytes);
 
         let window = unsafe {
             window_create(
-                this.title.as_ptr() as *const i8,
+                CString::new(this.title.as_str()).unwrap().as_ptr(),
                 size.clone(),
                 window_flag_t::WINDOW_FLAG_DOUBLE_BUFFERED
                     | (if config.fullscreen {
@@ -115,15 +115,134 @@ impl Why2025BadgeWindow {
         this
     }
 
+    /// Flush the window to the screen
     pub fn flush(&mut self) {
         unsafe { window_present(self.window, false, null_mut(), 0) };
+    }
+
+    /// Change the size of the window
+    pub fn resize(&mut self, size: Size) {
+        let new_size = window_size_t {
+            w: size.width as i32,
+            h: size.height as i32,
+        };
+        unsafe {
+            window_size_set(self.window, new_size);
+            window_framebuffer_size_set(self.window, new_size);
+        }
+    }
+
+    /// Get the position of the window
+    pub fn get_position(&mut self) -> (u32, u32) {
+        let position = unsafe { window_position_get(self.window) };
+        (position.x as u32, position.y as u32)
+    }
+    /// Set the position of the window
+    pub fn set_position(&mut self, position: (u32, u32)) {
+        unsafe {
+            window_position_set(
+                self.window,
+                window_coords_t {
+                    x: position.0 as i32,
+                    y: position.1 as i32,
+                },
+            );
+        }
+    }
+
+    /// Check if the window is in fullscreen mode
+    pub fn is_fullscreen(&self) -> bool {
+        let flags = unsafe { window_flags_get(self.window) };
+        flags & window_flag_t::WINDOW_FLAG_FULLSCREEN != window_flag_t::WINDOW_FLAG_NONE
+    }
+    /// Set the window to fullscreen mode
+    pub fn set_fullscreen(&mut self, fullscreen: bool) -> bool {
+        let flags = unsafe { window_flags_get(self.window) };
+        let old_fullscreen =
+            flags & window_flag_t::WINDOW_FLAG_FULLSCREEN != window_flag_t::WINDOW_FLAG_NONE;
+        if old_fullscreen == fullscreen {
+            return fullscreen; // No change needed
+        }
+        let flags = unsafe {
+            window_flags_set(
+                self.window,
+                window_flag_t(flags.0 ^ window_flag_t::WINDOW_FLAG_FULLSCREEN.0),
+            )
+        };
+        return flags & window_flag_t::WINDOW_FLAG_FULLSCREEN != window_flag_t::WINDOW_FLAG_NONE;
+    }
+    /// Check if the window is undecorated (no title bar, no borders)
+    pub fn is_undecorated(&self) -> bool {
+        let flags = unsafe { window_flags_get(self.window) };
+        flags & window_flag_t::WINDOW_FLAG_UNDECORATED != window_flag_t::WINDOW_FLAG_NONE
+    }
+    /// Set the window to be undecorated (no title bar, no borders)
+    pub fn set_undecorated(&mut self, undecorated: bool) -> bool {
+        let flags = unsafe { window_flags_get(self.window) };
+        let old_undecorated =
+            flags & window_flag_t::WINDOW_FLAG_UNDECORATED != window_flag_t::WINDOW_FLAG_NONE;
+        if old_undecorated == undecorated {
+            return undecorated; // No change needed
+        }
+        let flags = unsafe {
+            window_flags_set(
+                self.window,
+                window_flag_t(flags.0 ^ window_flag_t::WINDOW_FLAG_UNDECORATED.0),
+            )
+        };
+        return flags & window_flag_t::WINDOW_FLAG_UNDECORATED != window_flag_t::WINDOW_FLAG_NONE;
+    }
+    /// Check if the window is always on top
+    pub fn is_always_on_top(&self) -> bool {
+        let flags = unsafe { window_flags_get(self.window) };
+        flags & window_flag_t::WINDOW_FLAG_ALWAYS_ON_TOP != window_flag_t::WINDOW_FLAG_NONE
+    }
+    /// Set the window to always be on top of other windows
+    pub fn set_always_on_top(&mut self, always_on_top: bool) -> bool {
+        let flags = unsafe { window_flags_get(self.window) };
+        let old_always_on_top =
+            flags & window_flag_t::WINDOW_FLAG_ALWAYS_ON_TOP != window_flag_t::WINDOW_FLAG_NONE;
+        if old_always_on_top == always_on_top {
+            return always_on_top; // No change needed
+        }
+        let flags = unsafe {
+            window_flags_set(
+                self.window,
+                window_flag_t(flags.0 ^ window_flag_t::WINDOW_FLAG_ALWAYS_ON_TOP.0),
+            )
+        };
+        return flags & window_flag_t::WINDOW_FLAG_ALWAYS_ON_TOP != window_flag_t::WINDOW_FLAG_NONE;
+    }
+
+    /// Get the title of the window
+    pub fn get_title(&self) -> &str {
+        &self.title
+    }
+    /// Set the title of the window
+    pub fn set_title(&mut self, title: &str) {
+        self.title = title.to_string();
+        unsafe {
+            window_title_set(
+                self.window,
+                CString::new(self.title.as_str()).unwrap().as_ptr(),
+            );
+        }
+    }
+
+    /// Get the raw handle of the window.
+    ///
+    /// Marked unsafe, because you can cause funny memory issues
+    /// if you carelessly resize the window
+    pub unsafe fn raw_handle(&self) -> window_handle_t {
+        self.window
     }
 }
 impl OriginDimensions for Why2025BadgeWindow {
     fn size(&self) -> Size {
+        // let size = unsafe { window_size_get(self.window) };
         Size {
-            width: self.size.0 as u32,
-            height: self.size.1 as u32,
+            width: unsafe { (*self.framebuffer).w } as u32,
+            height: unsafe { (*self.framebuffer).h } as u32,
         }
     }
 }
@@ -136,11 +255,12 @@ impl DrawTarget for Why2025BadgeWindow {
     where
         I: IntoIterator<Item = Pixel<Self::Color>>,
     {
-        let canvas_width = self.size.0 as usize;
+        let size = self.size();
+        let canvas_width = size.width as usize;
         let backing = unsafe {
             core::slice::from_raw_parts_mut(
                 (*self.framebuffer).pixels,
-                self.size.0 as usize * self.size.1 as usize,
+                size.height as usize * size.width as usize,
             )
         };
 

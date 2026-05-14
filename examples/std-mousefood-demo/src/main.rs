446652b1d479d54b88f7645b32cd0e351b8933ad
Zebreus <zebreus@zebre.us>
2026-05-14 21:49:02 +0200

    Add std mousefood and ratatui demos

---
+++ examples/std-mousefood-demo/src/main.rs
@@ -0,0 +1,36 @@
+use embedded_graphics::{mono_font::ascii::FONT_7X13, prelude::*};
+use mousefood::{TerminalAlignment, prelude::*};
+use ratatui::{Frame, Terminal, widgets::Paragraph};
+use why2025_badge_embedded_graphics::Why2025BadgeWindow;
+
+fn main() {
+    let mut display = Why2025BadgeWindow::new_floating(
+        Size {
+            width: 200,
+            height: 200,
+        },
+        "Std Mousefood Demo",
+    );
+
+    let config = EmbeddedBackendConfig {
+        flush_callback: Box::new(|display: &mut Why2025BadgeWindow| {
+            display.flush();
+        }),
+        font_regular: FONT_7X13,
+        font_bold: None,
+        font_italic: None,
+        vertical_alignment: TerminalAlignment::Center,
+        horizontal_alignment: TerminalAlignment::Center,
+    };
+    let backend = EmbeddedBackend::new(&mut display, config);
+    let mut terminal = Terminal::new(backend).unwrap();
+
+    loop {
+        terminal.draw(draw).unwrap();
+    }
+}
+
+fn draw(frame: &mut Frame) {
+    let greeting = Paragraph::new("Hello World! (std mousefood demo)");
+    frame.render_widget(greeting, frame.area());
+}
---
+++ examples/std-ratatui-demo/src/main.rs
@@ -0,0 +1,31 @@
+use crate::app::App;
+use mousefood::{TerminalAlignment, prelude::*};
+use ratatui::Terminal;
+use why2025_badge_embedded_graphics::Why2025BadgeWindow;
+
+mod app;
+mod ui;
+
+fn main() {
+    let mut display = Why2025BadgeWindow::new_fullscreen();
+
+    let config = EmbeddedBackendConfig {
+        flush_callback: Box::new(|display: &mut Why2025BadgeWindow| {
+            display.flush();
+        }),
+        font_regular: embedded_graphics_unicodefonts::MONO_9X15,
+        font_bold: Some(embedded_graphics_unicodefonts::MONO_9X15_BOLD),
+        font_italic: None,
+        vertical_alignment: TerminalAlignment::Center,
+        horizontal_alignment: TerminalAlignment::Center,
+    };
+    let backend = EmbeddedBackend::new(&mut display, config);
+
+    let mut terminal = Terminal::new(backend).unwrap();
+    let mut app = App::new("Std RataTUI Demo", false);
+
+    loop {
+        terminal.draw(|frame| ui::render(frame, &mut app)).unwrap();
+        app.on_tick();
+    }
+}
